720bea32ee44879f4cccb8356946f601a794f78c
TeodorDyakov <tedy1996@gmail.com>
2021-06-13 20:11:11 +0300

    teo

--- tea.php
+++ tea.php
@@ -1,42 +1,78 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-       
-     $conn = new PDO("mysql:host=$host;dbname=chat", $username, $password);
-     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$host = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new PDO("mysql:host=$host;dbname=chat", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+header('Content-Type: application/json');
+$user_id = "";
+if (!isset($_COOKIE['id'])) {
+  $user_id = 'guest_' . generateRandomString(7);
+  setcookie('id', $user_id);
+} else {
+  $user_id = $_COOKIE['id'];
+}
+
+try {
+  $requestUrl = $_SERVER['REQUEST_URI'];
+  if (strpos($requestUrl, '/lastMsg')) {
+
+    $lastMsgId = $_GET["id"];
+
+    $sql = "";
    
-     header('Content-Type: application/json');
+    if (strpos($requestUrl, '/rand_match')){
+      $sql = "SELECT * FROM MESSAGES WHERE id > '" . $lastMsgId . "'";      
+    }else{
+      $sql = "SELECT * FROM MESSAGES WHERE id > '" . $lastMsgId . "'";
+    }
 
-     $requestUrl = $_SERVER['REQUEST_URI'];
-     if (strpos($requestUrl,'/lastMsg')) {
+    $query = $conn->query($sql) or die("failed!");
 
-      $lastMsgId = $_GET["id"];
-       
-      $sql = "SELECT * FROM MESSAGES WHERE id > '".$lastMsgId."'";
+    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
 
-      $query = $conn->query($sql) or die("failed!");
-       
-       $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($rows);
 
-       echo json_encode($rows);
+    $date = date('Y-m-d H:i:s');
 
-     }
-      else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-       
-       $sql = "SELECT * FROM MESSAGES";
-       
-       $query = $conn->query($sql) or die("failed!");
-       
-       $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+    $sql = "INSERT INTO last_active (id_of_user, last_active) 
+    VALUES ('{$user_id}', '{$date}')
+    ON DUPLICATE KEY UPDATE
+    last_active = '{$date}'";
 
-       echo json_encode($rows);
-     }else if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $text = json_decode(file_get_contents('php://input'), true);
-             
-        $sql = "INSERT INTO MESSAGES (text, author) 
-        VALUES ('{$text}', 'guest')";
+    $query = $conn->query($sql) or die("failed!");
 
-        $query = $conn->query($sql) or die("failed!");  
-     }
-?>
+  } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    $sql = "SELECT * FROM MESSAGES";
+
+    $query = $conn->query($sql) or die("failed!");
+
+    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($rows);
+  } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $text = json_decode(file_get_contents('php://input'), true);
+
+    $sql = "INSERT INTO MESSAGES (text, author) 
+        VALUES ('{$text}', '{$user_id}')";
+
+    $query = $conn->query($sql) or die("failed!");
+  }
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+function generateRandomString($length = 10)
+{
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $charactersLength = strlen($characters);
+  $randomString = '';
+  for ($i = 0; $i < $length; $i++) {
+    $randomString .= $characters[rand(0, $charactersLength - 1)];
+  }
+  return $randomString;
+}
+
